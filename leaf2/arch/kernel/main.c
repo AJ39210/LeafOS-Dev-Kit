cb866a773151bc49fec05abdcbe544e4722e3087
AJ <168369237+AJ39210@users.noreply.github.com>
2025-10-24 21:27:52 +0300

    Delete leaf2 directory

--- leaf2/arch/kernel/main.c
+++
@@ -1,6 +0,0 @@
-void kmain(void) {
-    volatile unsigned short* vga = (unsigned short*)0xB8000;
-    vga[0] = (0x0F << 8) | 'K'; // prints 'K' white
-    vga[1] = (0x0F << 8) | 'E'; // prints 'E' white
-    for (;;) __asm__ __volatile__("hlt");
-}
