72cfc382e65ce3dd024e532b7431f1d0e08f340a
AJ <168369237+AJ39210@users.noreply.github.com>
2025-10-26 00:00:31 +0300

    Update keyboard.h

--- keyboard/keyboard.h
+++ keyboard/keyboard.h
@@ -1,21 +1,11 @@
+// include/keyboard.h
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-#include "types.h"
+#include <stdint.h>
 
-#define KEYBOARD_DATA_PORT 0x60
-#define KEYBOARD_STATUS_PORT 0x64
-
-// Keyboard initialization
 void keyboard_init(void);
-
-// Get a character from keyboard (blocking)
 char keyboard_getchar(void);
-
-// Check if key is available
-int keyboard_available(void);
-
-// Read scancode
-uint8_t keyboard_read_scancode(void);
+void keyboard_wait_key(void);
 
 #endif
