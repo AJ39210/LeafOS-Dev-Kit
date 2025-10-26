865155fd958a628e7157d96fa702ef22667ab970
AJ <168369237+AJ39210@users.noreply.github.com>
2025-10-26 09:05:48 +0200

    Add files via upload

---
+++ leaf/include/cmds.h
@@ -0,0 +1,10 @@
+// include/cmds.h
+#ifndef CMDS_H
+#define CMDS_H
+
+void cmd_help(const char* args);
+void cmd_about(const char* args);
+void cmd_echo(const char* args);
+void cmd_clear(const char* args);
+
+#endif
